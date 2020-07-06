83953ef4b222a572ab00ac6b342af8abd966ef4e
Alexander Rezvov <rezvov@majordomo.ru>
2020-07-06 14:35:52 +0300

    Version fix

--- pkgs/drupal/default.nix
+++ pkgs/drupal/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "drupal";
-  version = "9.0.1";
+  version = "8.9.1";
   src = fetchurl {
     url = "https://ftp.drupal.org/files/projects/${pname}-${version}.tar.gz";
-    sha256 = "83aec8ffad7358b340818759418ce4500b45871fc1311ca9d11f8ce4cd9871c4";
+    sha256 = "2c4460b04faade7440103c683340525a450f9b080ff75af7af4ddc670b73eefb";
   };
   installPhase = ''
     tar czf ${pname}-${version}.tar.gz *
